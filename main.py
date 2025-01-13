42252f3de0f8aac2161e661d717da4ae3ec78501
Saccadence <aurolfe035@gmail.com>
2025-01-13 13:40:50 -0800

    main.py - Added window generation

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame       # Pygame library (source venv/bin/activate)
 import constants    # Constants.py library
-
+from constants import *
 
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init     # Start pygame
+    
+    # Define 'screen' (process window size) 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game Loop
+    while True:
+        screen.fill((0, 0, 0))  # Fill screen with black
+        pygame.display.flip()   # Refresh screen
+
+
     
 
 if __name__ == "__main__":
