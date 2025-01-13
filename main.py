9a36bfe8a03b7563676a725991fd399a6800886f
Saccadence <aurolfe035@gmail.com>
2025-01-13 14:48:22 -0800

    Added 'player' rotation

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        # Movement
+        player.update(dt)
         
         # Render Methods
         screen.fill((0, 0, 0))  # Fill screen with black
