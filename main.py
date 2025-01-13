efe6509e6d7be3f5f227b2ad62c0150709e834d5
Saccadence <aurolfe035@gmail.com>
2025-01-13 13:53:19 -0800

    main.py - Added frame limiter

--- main.py
+++ main.py
@@ -7,13 +7,27 @@ def main():
     print("Starting asteroids!")
     pygame.init     # Start pygame
     
+    # Delta Time - Frame Limiter
+    clock = pygame.time.Clock()
+    dt = 0
+    
     # Define 'screen' (process window size) 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     # Game Loop
     while True:
+        
+        # Terminate process (QUIT)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill((0, 0, 0))  # Fill screen with black
         pygame.display.flip()   # Refresh screen
+        
+        # Establish Frame Limit
+        # Store time since last called (in seconds)
+        dt = clock.tick(60) / 1000
 
 
     
