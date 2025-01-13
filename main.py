7f66fb689f442864b1924c39588c69f1e10e8075
Saccadence <aurolfe035@gmail.com>
2025-01-13 09:20:18 -0800

    New - constants.py | Updated - main.py, .gitignore

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import pygame	# Used to access pygame library
+import pygame       # Pygame library (source venv/bin/activate)
+import constants    # Constants.py library
 
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     
 
 if __name__ == "__main__":
